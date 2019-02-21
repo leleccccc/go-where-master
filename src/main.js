ae36e296d82e9939e7bee4849c687b5c293f4c41
leleccccc <songleledashuaibi@foxmail.com>
2019-02-21 17:05:32 +0800

    加入全局css和js配置

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/styles/reset.css' // 默认样式
+import '@/assets/styles/iconfont.css' // 阿里巴巴图库
+import FastClick from 'fastclick' // 处理移动端click事件300毫秒延迟
 
+FastClick.attach(document.body) // 处理移动端click事件300毫秒延迟
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
